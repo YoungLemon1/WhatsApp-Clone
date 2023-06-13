fcc9c14c467e864847303285e025316070c469dd
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-13 21:36:35 +0300

    users router changes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,11 +21,21 @@ userRouter.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const user = await UserModel.findById(id);
-    if (!user) {
-      return res.status(400).json({
-        error: "username or password are incorrect, please try again",
-      });
-    }
+    res.status(200).json({
+      data: user,
+    });
+  } catch (err) {
+    console.error(err.stack);
+    res.status(500).json({
+      error: "Internal server error: Failure fetching user",
+    });
+  }
+});
+
+userRouter.get("/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await UserModel.findById({ username: username });
     res.status(200).json({
       data: user,
     });
@@ -48,7 +58,7 @@ userRouter.post(
     const { name, username, birthdate, role } = req.body;
 
     // Check if username already exists in the database
-    const existingUser = await UserModel.findOne({ username });
+    const existingUser = await UserModel.findOne({ username: username });
     if (existingUser) {
       return res.status(400).json({
         error: "Username already exists",
@@ -82,22 +92,22 @@ userRouter.put(
     const { name, username, birthdate, role } = req.body;
 
     // Check if username already exists in the database
-    const existingUser = await UserModel.findOne({ _id: id });
-    if (!existingUser) {
+    const user = await UserModel.findOne({ _id: id });
+    if (!user) {
       return res.status(400).json({
         error: "Bad request: user does not exist",
       });
     }
     try {
-      existingUser.name = name;
-      existingUser.username = username;
-      existingUser.birthdate = birthdate;
-      existingUser.role = role;
+      user.name = name;
+      user.username = username;
+      user.birthdate = birthdate;
+      user.role = role;
 
-      await existingUser.save();
+      await user.save();
 
       res.status(200).json({
-        data: existingUser,
+        data: user,
       });
     } catch (error) {
       console.error(error);
