7d487e0094efc146c60e1002237a0d7b08a2aa11
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-13 23:22:45 +0300

    initial user page

--- frontend/src/components/user-page.js
+++ frontend/src/components/user-page.js
@@ -2,13 +2,18 @@ import React from "react";
 import moment from "moment";
 
 function UserPage({ user }) {
+  function dateFormat(date) {
+    if (date) {
+      return moment(date).format("DD-MM-YYYY");
+    } else return "";
+  }
   return (
     <div>
       <h1>{user.username}</h1>
       <p>id: {user._id}</p>
       <p>name: {user.name}</p>
       <p>username: {user.username}</p>
-      <p>birthdate: {moment(user.birthdate).format("DD-MM-YYYY")}</p>
+      <p>birthdate: {dateFormat(user.birthdate)}</p>
     </div>
   );
 }
