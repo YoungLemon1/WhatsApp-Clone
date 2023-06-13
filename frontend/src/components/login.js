a76be93f5f94ccaa7589d6fd05856602e19eaedf
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-14 00:13:36 +0300

    coniguration of react router

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Button, Container, Alert } from "react-bootstrap";
+import { Button, Container, Link } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Axios from "axios";
 function Login({ setUser, setLoggedIn }) {
   const [usernameFooter, setUsernameFooter] = useState();
   const [username, setUsername] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
   async function loginUser() {
     if (!username) {
       console.log("Error: empty username");
@@ -32,11 +34,14 @@ function Login({ setUser, setLoggedIn }) {
       setError("An error occurred. Please try again later.");
     }
   }
+  function routeToSignup() {
+    navigate("/");
+  }
   return (
-    <Container id="login">
-      <h1>Chat and Play</h1>
-      <Container id="error-container">{error}</Container>
-      <Container>
+    <div>
+      <form id="login" className="user-form">
+        <h1>Chat and Play</h1>
+        <Container id="error-container">{error}</Container>
         <label htmlFor="username">Username</label>
         <input
           id="username"
@@ -47,15 +52,23 @@ function Login({ setUser, setLoggedIn }) {
           }}
         ></input>
         <small>{usernameFooter}</small>
-      </Container>
-      <Container>
         <label htmlFor="password">Password</label>
         <input id="password" name="password" disabled></input>
-      </Container>
-      <Button id="login" name="login" variant="success" onClick={loginUser}>
-        login
-      </Button>
-    </Container>
+        <div>
+          <Button id="login" name="login" variant="success" onClick={loginUser}>
+            login
+          </Button>
+          <Button
+            id="signup"
+            name="signup"
+            variant="primary"
+            onClick={routeToSignup}
+          >
+            signup
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
 
