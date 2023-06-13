5a0f07301f719e8168cc1047cb8c5324a7a04eca
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-13 19:17:33 +0300

    changed app.js to a login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Navbar, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import Axios from "axios";
-
 import "react-datepicker/dist/react-datepicker.css";
 import AllUsers from "./components/AllUsers";
 import NewUser from "./components/NewUser";
@@ -9,25 +10,29 @@ import NewUser from "./components/NewUser";
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    Axios.get("http://localhost:5000/users")
-      .then((res) => {
-        setUsers(res.data.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  useEffect(() => {}, []);
 
   const addUser = (user) => {
     setUsers([...users, user]);
   };
 
   return (
-    <div className="App">
-      <AllUsers users={users}></AllUsers>
-      <NewUser users={users} addUser={addUser} />
-    </div>
+    <Container className="App">
+      <Container id="login">
+        <h1>Chat and Play</h1>
+        <Container>
+          <label id="username">Username</label>
+          <input id="username" name="username"></input>
+        </Container>
+        <Container>
+          <label id="password">Password</label>
+          <input id="password" name="password" disabled></input>
+        </Container>
+        <Button id="login" name="login" variant="success">
+          login
+        </Button>
+      </Container>
+    </Container>
   );
 }
 
