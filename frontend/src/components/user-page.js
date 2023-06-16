57d7d7838efa0a05cfa90a92dc5869c7dbab1b71
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-16 20:05:11 +0300

    commented date format, added chat history plcaeholder

--- frontend/src/components/user-page.js
+++ frontend/src/components/user-page.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 function UserPage({ user }) {
-  function dateFormat(date) {
+  const [chats, setChats] = useState();
+  /*function dateFormat(date) {
     if (date) {
       return moment(date).format("DD-MM-YYYY");
     } else return "";
-  }
+  }*/
   return (
     <div>
       <h1>{user.username}</h1>
-      <div id="chat-history"></div>
+      <div id="chat-history">{chats.map()}</div>
     </div>
   );
 }
