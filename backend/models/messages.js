74e138f34c820fedffce82d578883a948c1415cb
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-22 02:11:10 +0300

    removed recepient

--- backend/models/messages.js
+++ backend/models/messages.js
@@ -5,9 +5,6 @@ const MessageSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
-  recipient: {
-    type: Schema.Types.ObjectId,
-  },
   text: {
     type: String,
   },
