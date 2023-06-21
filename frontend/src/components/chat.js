791578e384a9ef17bcf9a8aa25dcbfb12fa22200
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-21 19:45:50 +0300

    added to chat component

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
-function Chat() {
-  const { id } = useParams();
-  const [loggedUser, SetLoggedUser] = useState("");
-  <div>
-    <h1>Chat Room: {id}</h1>
-    {/* Rest of the component */}
-  </div>;
+function Chat({
+  chat,
+  setCurrentChat,
+  loggedUser,
+  isUserInChatroom,
+  setIsUserInChatroom,
+}) {
+  const [messages, setMessages] = useState([]);
+  function exitChat() {
+    console.log(isUserInChatroom);
+    setCurrentChat({});
+    setIsUserInChatroom(false);
+    return;
+  }
+  return (
+    <div>
+      <div id="chat-header">
+        <button onClick={exitChat}>←</button>
+        <img src={chat.imageURL} alt="chat profile"></img>
+        <h1>{chat.name}</h1>
+      </div>
+      <div id="chat-content">
+        {messages.map((message) => {
+          return <div key={message._id}></div>;
+        })}
+      </div>
+      <div id="chat-footer">
+        <input id="message-text"></input>
+        <Button>Send</Button>
+      </div>
+    </div>
+  );
 }
 export default Chat;
