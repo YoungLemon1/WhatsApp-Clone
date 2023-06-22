de99c2c42528500713b7779031e449ed5f6708de
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-22 10:42:56 +0300

    create chat if there are no messages

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -14,8 +14,16 @@ function Chat({
   const [messageText, setMessageText] = useState("");
 
   useEffect(() => {
-    const res = Axios.get();
-  }, [messages.length]);
+    try {
+      const res = Axios.get(
+        `http://localhost:5000/messages/chatroom/${chat.id}`
+      );
+      const data = res.data;
+      setMessages(data);
+    } catch (error) {
+      console.error("Failed to fetch messages", error);
+    }
+  }, [chat.id]);
 
   function exitChat() {
     console.log(isUserInChatroom);
@@ -26,6 +34,7 @@ function Chat({
       setChatHistory(updatedChatHistory);
     }
   }
+
   async function sendMessage() {
     const message = {
       sender: loggedUser._id,
@@ -33,9 +42,16 @@ function Chat({
       createdAt: Date.now(),
     };
     try {
+      if (messages.length === 0) {
+        const res = await Axios.post("http://localhost:5000/chatrooms", chat);
+        const data = res.data;
+        console.log("chatroom created", data);
+        chat.id = data._id;
+      }
       const res = await Axios.post("http://localhost:5000/messages", message);
       const data = res.data;
       console.log("message sent", data);
+      setMessages([...messages, message]);
       setMessageText("");
       const textInput = document.getElementById("message-text");
       textInput.value = "";
@@ -54,7 +70,8 @@ function Chat({
       </div>
       <div className="chat-body">
         {messages.map((message) => {
-          return <div key={message._id}></div>;
+          let sender;
+          return <div key={message._id} className={sender}></div>;
         })}
       </div>
       <div className="chat-footer">
