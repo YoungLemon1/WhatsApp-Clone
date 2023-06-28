2738008e9be0d6afc4cbec2f117f1a21ed3184c6
yaniv <yaniv@DESKTOP-K7C29F9>
2023-06-29 01:22:34 +0300

    updated app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,18 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import Login from "./components/login";
 import UserPage from "./components/user-page";
-import Signup from "./components/signup";
-import Chat from "./components/chat";
 
 function App() {
   const [user, setUser] = useState(null);
 
-  const homeRoute = () => {
-    return user ? (
-      <UserPage user={user} setUser={setUser} />
-    ) : (
-      <Login setUser={setUser} />
-    );
-  };
-
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={homeRoute()} />
-          <Route path="/chat/:id" element={<Chat />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  if (user) {
+    return <UserPage user={user} setUser={setUser} />;
+  } else {
+    return <Login setUser={setUser} />;
+  }
 }
 
 export default App;
