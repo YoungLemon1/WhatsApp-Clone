aa9bd3d89a33c5b4460515ec4cf2054825d7664d
yaniv <yaniv@DESKTOP-K7C29F9>
2023-07-01 23:54:13 +0300

    cahnged oreder of routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,19 @@ const userRouter = Router();
 
 userRouter.get("/", async (req, res) => {
   try {
+    const { username } = req.query;
+    const user = await UserModel.findOne({ username: username });
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err.stack);
+    res.status(500).json({
+      error: "Internal server error: Failure fetching user",
+    });
+  }
+});
+
+userRouter.get("/", async (req, res) => {
+  try {
     const users = await UserModel.find({});
     res.status(200).json(users);
   } catch (err) {
@@ -25,19 +38,6 @@ userRouter.get("/:id", async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    res.status(200).json(user);
-  } catch (err) {
-    console.error(err.stack);
-    res.status(500).json({
-      error: "Internal server error: Failure fetching user",
-    });
-  }
-});
-
-userRouter.get("/", async (req, res) => {
-  try {
-    const { username } = req.query;
-    const user = await UserModel.findOne({ username: username });
     res.status(200).json(user);
   } catch (err) {
     console.error(err.stack);
