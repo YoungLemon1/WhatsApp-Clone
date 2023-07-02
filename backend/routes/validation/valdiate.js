9bf0193d365772bf9d5c0438ab29bcd0f7989cd7
yaniv <yaniv@DESKTOP-K7C29F9>
2023-07-02 18:20:37 +0300

    validate function in it's own file

---
+++ backend/routes/validation/valdiate.js
@@ -0,0 +1,9 @@
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+export default validate;
