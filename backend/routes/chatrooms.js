0aaf0f7301ab505668ef3d9509127c0f6dfc2d00
yaniv <yaniv@DESKTOP-K7C29F9>
2023-07-02 19:19:49 +0300

    imported validate to chatroom

--- backend/routes/chatrooms.js
+++ backend/routes/chatrooms.js
@@ -1,17 +1,8 @@
 import { Router } from "express";
 import ChatRoom from "../models/chatroom.js";
-import { body, validationResult } from "express-validator";
-import User from "../models/user.js";
+import { body } from "express-validator";
+import validate from "./validation/valdiate.js";
 const chatRoomRouter = Router();
-
-// Validation middleware
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
-};
 
 chatRoomRouter.get("/", async (req, res) => {
   try {
