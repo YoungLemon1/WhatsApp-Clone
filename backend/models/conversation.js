183ffceccd44e78b2f3e678a7a345ef3530c0c23
yaniv <yaniv@DESKTOP-K7C29F9>
2023-07-03 21:43:45 +0300

    conversation model

---
+++ backend/models/conversation.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const ConversationSchema = new Schema({
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  lastUpdatedAt: {
+    type: Date,
+  },
+});
+
+const Conversation = model("Conversation", ConversationSchema);
+
+export default Conversation;
