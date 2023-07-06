86a9bf65b4a822fa989487be2eb464f9f4de1769
yaniv <yaniv@DESKTOP-K7C29F9>
2023-07-06 09:46:03 +0300

    matched versions of socket.io changed app listen to server listen

--- backend/index.js
+++ backend/index.js
@@ -15,15 +15,15 @@ const app = express();
 const PORT = parseInt(process.env.PORT || "5000");
 const server = createServer(app);
 
+app.use(cors());
+app.use(express.json());
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
-
-app.use(cors());
-app.use(express.json());
 
 io.on("connection", (socket) => {
   console.log(socket.id + " connected");
@@ -42,6 +42,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(socket.id + " disconnected");
   });
+  socket.on("test", (data) => console.log(data));
 });
 
 mongoose
@@ -81,6 +82,6 @@ app.use((req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
